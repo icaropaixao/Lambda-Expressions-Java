d981ca6caf1b7e0109c086ca0f61923fa215ecea
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-11-22 20:51:39 -0300

    Aula 1 sobre Expressões lambda

---
+++ src/funcao_anonima/introducao/application/Program.java
@@ -0,0 +1,24 @@
+package funcao_anonima.introducao.application;
+
+import funcao_anonima.introducao.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        // expressão lambda diretamente no argumento
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
