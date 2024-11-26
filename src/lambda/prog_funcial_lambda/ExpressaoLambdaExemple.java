48dc4cf79bdcbcdd1508456f8f9c95d335f060e0
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-11-25 21:28:55 -0300

    Exemplos de Expressão Lambda

--- src/lambda/prog_funcial_lambda/ExpressaoLambdaExemple.java
+++ src/lambda/prog_funcial_lambda/ExpressaoLambdaExemple.java
@@ -1,36 +1,32 @@
 package lambda.prog_funcial_lambda;
 
 import lambda.funcao_anonima.introducao.entities.Product;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class FuncoesPrimeiraOrdem {
+public class ExpressaoLambdaExemple {
 
-    public static int compareProducts(Product p1, Product p2) {=
+    //O que são "expressões lambda"?
+    /*
+    Emprogramação funcional, expressão lambda corresponde a uma
+     funçãoanônimadeprimeiraclasse.
+     */
+    public static int compareProducts(Product p1, Product p2) {
         return p1.getPrice().compareTo(p2.getPrice());
-
     }
 
-    // METODO REFERENCIAL
-    /*
-     Utilizamos aqui
-     "method references"
-         Operador ::
-
-         Sintaxe:
-         Classe::método
-     */
     public static void main(String[] args) {
-
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("TV", 900.00));
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        //            Classe           |   Método
-        list.sort(FuncoesPrimeiraOrdem::compareProducts);
+
+        list.sort(ExpressaoLambdaExemple::compareProducts);
+        list.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice())); // expressão lambda
+
         list.forEach(System.out::println);
     }
-
 }
